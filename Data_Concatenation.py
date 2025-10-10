e46b19977046af60d13b8fa0390a71fff70e401d
joralt <altmanrj@gmail.com>
2025-10-10 05:26:22 -0500

    Update Data_Concatenation.py

--- Data_Concatenation.py
+++ Data_Concatenation.py
@@ -20,6 +20,12 @@ DateTimeNum = 1
 
 patientPath = #Jordan you need to link to ELIAS
 
+LeftAll = [] #A list of 2D matrices that contain all electrodes for each 20 min interval
+LeftAll_avg = []
+
+RightAll = []
+RightAll_avg = []
+
 prompt = 'Intervals of 10 or 20 (1/2): '
 interv = int(input(prompt))
 
@@ -72,26 +78,40 @@ for i, folder in enumerate(fileList):
                    leftDownArr = decimate(ns3Data.Data[LeftElectrodeIDs,:],4, axis=1)
                    df = pd.DataFrame(leftDownArr)
                    leftDownArr = df.interpolate(method='linear', axis=1) #interpolate across NaNs
-
+                   leftDownArr = np.array(leftDownArr)
+                   leftDownArr_avg = np.mean(leftDownArr, axis=0)
+            
                    rightDownArr = decimate(ns3Data.Data[RightElectrodeIDs,:],4, axis=1)
                    df = pd.DataFrame(rightDownArr)
                    rightDownArr = df.interpolate(method='linear',axis=1) #interpolate across NaNs
-                   
+                   rightDownArr = np.array(rightDownArr)
+                   rightDownArr_avg = np.mean(rightDownArr, axis=0)
+
                if fs == 30000:
                    leftDownArr = decimate(ns3Data.Data[LeftElectrodeIDs,:],60, axis=1)
                    df = pd.DataFrame(leftDownArr)
                    leftDownArr = df.interpolate(method='linear',axis =1) #interpolate across NaNs
+                   leftDownArr = np.array(leftDownArr)
+                   leftDownArr_avg = np.mean(leftDownArr, axis=0)
 
                    rightDownArr = decimate(ns3Data.Data[RightElectrodeIDs,:],60, axis=1)
                    df = pd.DataFrame(rightDownArr)
                    rightDownArr = df.interpolate(method='linear',axis=1) #interpolate across NaNs
+                   rightDownArr = np.array(rightDownArr)
+                   rightDownArr_avg = np.mean(rightDownArr, axis=0)
 
                #FIND way to organize all data
-               
+               LeftAll_avg.append(leftDownArr_avg) #A list of 2D matrices that contain all electrodes for each 20 min interval
+               LeftAll.append(leftDownArr)
+
+               RightAll_avg.append(rightDownArr_avg)
+               RightAll.append(rightDownArr)
 
            except Exception as e:
                del LeftLFPArray
                continue
+           
+           
                
             
     
